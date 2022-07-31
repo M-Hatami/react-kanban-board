08df47ec439c091f7b119cba1fb9f5f5565c8985
MH <32916645+M-Hatami@users.noreply.github.com>
2022-07-31 11:46:19 +1000

    add color picker component

--- src/Presentational/ColorPicker.js
+++ src/Presentational/ColorPicker.js
@@ -1,7 +1,19 @@
 import React, { useState } from "react";
 
+const colorNames = [
+  "Aquamarine",
+  "BlueViolet",
+  "Chartreuse",
+  "CornflowerBlue",
+  "Thistle",
+  "SpringGreen",
+  "SaddleBrown",
+  "PapayaWhip",
+  "MistyRose",
+];
+
 export default function ColorPicker() {
-  const [color, setColor] = useState();
+  const [color, setColor] = useState("#A5E457");
 
   const divStyle = { backgroundColor: color };
   const grn = {
@@ -36,6 +48,12 @@ export default function ColorPicker() {
       <button style={ylw} onClick={() => setColor("#FFF289")}></button>
       <button style={red} onClick={() => setColor("#FEB389")}></button>
       <button style={blu} onClick={() => setColor("#89E2FF")}></button>
+      <span> ==یا== </span>
+      {colorNames.map((colorName) => (
+        <button onClick={() => setColor(colorName)} key={colorName}>
+          {colorName}
+        </button>
+      ))}
     </div>
   );
 }
